ee22f1c4291f9cec478aab0a511bc3b60552d72a
xanadoom <98607993+xanadoom@users.noreply.github.com>
2023-02-03 09:36:55 +0100

    aa

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,28 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+//middleware
+
+app.use(express.json())
+
+app.get('/hello', (req, res) => res.send('Hello World!'))
+
+app.use('/api/vs1/tasks', tasks)
+
+const port = 3000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,48 @@
+const task = require('../model/task')
+const Task = require('../model/Task')
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+}
+
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+}
+
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOne({ _id: taskID })
+    if (!task) {
+      return res.status(404).json({ msg: `No task whit id : ${taskID}` })
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
+}
+
+const updateTask = (req, res) => {
+  res.send('update task')
+}
+
+const deleteTask = (req, res) => {
+  res.send('delete task')
+}
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+}
